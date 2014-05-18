fb365e62c76e9d5f467f0356a49156b979fd6e34
Atsuya Takagi <asoftonight@gmail.com>
2014-05-18 16:50:45 +0900

    keep it awake

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
+app.get('/ping', function(req, res){
+  res.send('OK');
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function listen() {
   console.log("Started on " + port);
 });
+
+// keep it up
+var url = process.env.HEROKU_URL || 'http://localhost:' + port + '/ping';
+var Heartbeater = require('wakeitup');
+var heartbeater = new Heartbeater(url);
+setInterval(function () { heartbeater.run(); }, 1000 * 60 * 2);
