e38b5e59d756ae8c6196780c79413ae6ad7f546b
Atsuya Takagi <asoftonight@gmail.com>
2014-05-18 15:47:44 +0900

    fix console log

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ app.get('/', function(req, res){
 });
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port);
-console.log("Started on " + port);
+app.listen(port, function listen() {
+  console.log("Started on " + port);
+});
